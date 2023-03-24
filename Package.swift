1430ff952f2f015dae99fc2a302661b8e6e1ebd5
VietDzung <dzungpv@outlook.com>
2023-03-24 14:12:03 +0700

    Update binary patch

--- Package.swift
+++ Package.swift
@@ -41,27 +41,27 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GoogleMaps",
-            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.0/GoogleMaps.xcframework.zip",
+            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.1/GoogleMaps.xcframework.zip",
             checksum: "b9be6db3f72a92d730b5dd7e04be75b3c87692fbc409e9064548a80beabe2b12"
         ),
         .binaryTarget(
             name: "GoogleMapsBase",
-            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.0/GoogleMapsBase.xcframework.zip",
+            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.1/GoogleMapsBase.xcframework.zip",
             checksum: "cb9fbfb9d8d197899b9bb4ea0d40918718cd1ba36d810dfa8cde37f8ab69ce77"
         ),
         .binaryTarget(
             name: "GoogleMapsCore",
-            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.0/GoogleMapsCore.xcframework.zip",
+            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.1/GoogleMapsCore.xcframework.zip",
             checksum: "498dbb3602976185286e7eaf4d4af3015655a67116df63c81d4512eb8b04cc39"
         ),
         .binaryTarget(
             name: "GoogleMapsM4B",
-            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.0/GoogleMapsM4B.xcframework.zip",
+            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.1/GoogleMapsM4B.xcframework.zip",
             checksum: "00a6b1bb84c03e3969dfad19904e6c2c9a508dbba0252533e036681c1279fc9d"
         ),
         .binaryTarget(
             name: "GooglePlaces",
-            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.0/GooglePlaces.xcframework.zip",
+            url: "https://github.com/dzungpv/GoogleMaps-SP/releases/download/7.4.1/GooglePlaces.xcframework.zip",
             checksum: "63fd4951999036f4d4fa978339ea8db778095da07765e6cfb7e0153cca921915"
         )
     ]
